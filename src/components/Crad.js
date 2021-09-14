c70466460410af7339f5ee38e57467d3356e620d
Rohitky <Rohitky34@gmail.com>
2021-09-13 23:17:58 -0700

    added let a

--- src/components/Crad.js
+++ src/components/Crad.js
@@ -1,6 +1,7 @@
 import "./Card.css";
 function Card(props) {
     const classes = 'card ' + props.className;
+    let a = 0;
   return <div className={classes}>{props.children}</div>;
 }
 export default Card;
